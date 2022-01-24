04de2831444bc873255b1b4c39dea81db3c558ff
Tavi Selim <oselim14@gmail.com>
2022-01-24 14:50:59 -0800

    users page

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,28 +1,36 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { checkToken } from '../../utilities/users-service';
 import * as userAPI from '../../utilities/users-api';
+import { Link } from 'react-router-dom';
 
-export default function SearchPage() {
+export default function SearchPage(props) {
 
   const [otherUsers, setOtherUsers] = useState([]);
 
 
-  useEffect(function(){
-    async function getAllUsers(){
+  useEffect(function () {
+    async function getAllUsers() {
       const users = await userAPI.getAll();
       setOtherUsers(users);
       console.log(users);
     }
     getAllUsers();
   }, []);
-  
+
 
   return (
     <>
       <h1>Make a Friend</h1>
       <div>
         {otherUsers.map((u, idx) =>
-          <h1>{u.name}</h1>
+          <div>
+            <Link to={`/users/${idx}`}>
+              <div>
+                <h1>{u.name}</h1>
+                <img src={u.profileImage} alt={u.name} width='100px' />
+              </div>
+            </Link>
+          </div>
         )}
       </div>
     </>
